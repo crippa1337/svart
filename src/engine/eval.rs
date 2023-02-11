dbe75813ec8ad46ef1aa5f9be4388dca0dacd16b
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2023-02-11 14:58:48 +0100

    Simplified eval code

--- src/engine/eval.rs
+++ src/engine/eval.rs
@@ -19,31 +19,25 @@ pub fn evaluate(board: &Board) -> i16 {
     let mut eg = 0;
     let mut game_phase = 0;
 
-    for pt in Piece::ALL {
-        for square in board.pieces(pt) {
-            let piece = board.piece_on(square).unwrap();
-            let color = board.color_on(square).unwrap();
-            let sq = square.flip_rank() as usize;
-            game_phase += PHASE_INC[p_type(piece)];
+    for square in board.occupied() {
+        let piece = board.piece_on(square).unwrap();
+        let color = board.color_on(square).unwrap();
+        let sq = square.flip_rank() as usize;
+        game_phase += PHASE_INC[p_type(piece)];
 
-            match color {
-                Color::White => {
-                    mg += MG_TABLE[p_type(piece)][sq];
-                    eg += EG_TABLE[p_type(piece)][sq];
-                }
-                Color::Black => {
-                    mg -= MG_TABLE[p_type(piece) + 6][sq];
-                    eg -= EG_TABLE[p_type(piece) + 6][sq];
-                }
+        match color {
+            Color::White => {
+                mg += MG_TABLE[p_type(piece)][sq];
+                eg += EG_TABLE[p_type(piece)][sq];
+            }
+            Color::Black => {
+                mg -= MG_TABLE[p_type(piece) + 6][sq];
+                eg -= EG_TABLE[p_type(piece) + 6][sq];
             }
         }
     }
 
-    let mut mg_weight = game_phase;
-    if mg_weight > 24 {
-        mg_weight = 24
-    };
-
+    let mg_weight = std::cmp::min(game_phase, 24);
     let eg_weight = 24 - mg_weight;
 
     match board.side_to_move() {

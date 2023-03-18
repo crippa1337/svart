10ec0e9e0b9fd51cf91d28d4dc38f0bfb3f512ca
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2023-03-19 00:34:58 +0100

    okay we do it like this yes yes

--- src/engine/nnue/network.rs
+++ src/engine/nnue/network.rs
@@ -1,8 +1,24 @@
-const INPUT: i32 = 768;
-const HIDDEN: i32 = 256;
+use crate::constants::MAX_PLY;
+
+const FEATURES: usize = 768;
+const HIDDEN: usize = 256;
+
+// clipped relu
 const CR_MIN: i32 = 0;
-const CR_MAX: i32 = 1 * QA;
+const CR_MAX: i32 = 255;
+
+// quantization
+const QAB: i32 = 255 * 64;
 const SCALE: i32 = 400;
 
-const QA: i32 = 255;
-const QB: i32 = 64;
+struct Accumulator {
+    white: [i16; HIDDEN],
+    black: [i16; HIDDEN],
+}
+
+struct Parameters {
+    feature_weights: [i16; FEATURES * HIDDEN],
+    feature_bias: [i16; HIDDEN],
+    output_weights: [i16; HIDDEN * 2],
+    output_bias: i16,
+}

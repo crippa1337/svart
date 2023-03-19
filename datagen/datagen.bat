8a48c09181880998ea466e18bd331df5d7c4ad98
crippa1337 <casdcasd5@gmail.com>
2023-03-20 00:04:20 +0100

    update datagen script,tracking down this pesky bug

--- datagen/datagen.bat
+++ datagen/datagen.bat
@@ -1,10 +1,11 @@
 @echo off
 
 cd ..
+cargo b -r
 
 cutechess-cli -engine cmd=target/release/svart.exe -engine cmd=target/release/svart.exe ^
     -each proto=uci depth=7 tc=inf stderr=datagen/stderr.txt ^
-    -rounds 500 -concurrency 9 ^
+    -rounds 500 -concurrency 1 ^
     -openings file=datagen/UHO_XXL_2020.pgn format=pgn order=sequential plies=16 ^
     -pgnout datagen/games.pgn
 

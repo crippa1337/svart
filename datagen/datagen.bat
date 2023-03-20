1cfae43bd27925eec4cad0b2ac0481f0b3943027
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2023-03-20 14:44:08 +0100

    pesky pgn

--- datagen/datagen.bat
+++ datagen/datagen.bat
@@ -5,8 +5,9 @@ cargo b -r
 
 cutechess-cli -engine cmd=target/release/svart.exe -engine cmd=target/release/svart.exe ^
     -each proto=uci depth=7 tc=inf stderr=datagen/stderr.txt ^
-    -rounds 500 -concurrency 1 ^
-    -openings file=datagen/UHO_XXL_2020.pgn format=pgn order=sequential plies=16 ^
-    -pgnout datagen/games.pgn
+    -rounds 1 -concurrency 1 ^
+    -openings file=datagen/book.pgn format=pgn order=sequential plies=16 ^
+    -pgnout datagen/games.pgn ^
+    -debug
 
 cd datagen

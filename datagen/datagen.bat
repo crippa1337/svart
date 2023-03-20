526a4db0c92d548286ba583ba7061fd807cc8315
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2023-03-20 14:58:10 +0100

    ptc

--- datagen/datagen.bat
+++ datagen/datagen.bat
@@ -4,10 +4,9 @@ cd ..
 cargo b -r
 
 cutechess-cli -engine cmd=target/release/svart.exe -engine cmd=target/release/svart.exe ^
-    -each proto=uci depth=7 tc=inf stderr=datagen/stderr.txt ^
-    -rounds 1 -concurrency 1 ^
-    -openings file=datagen/book.pgn format=pgn order=sequential plies=16 ^
-    -pgnout datagen/games.pgn ^
-    -debug
+    -each proto=uci depth=7 tc=50 stderr=datagen/stderr.txt ^
+    -rounds 500 -concurrency 1 ^
+    -openings file=datagen/UHO_XXL_2020.pgn format=pgn order=sequential plies=16 ^
+    -pgnout datagen/games.pgn
 
 cd datagen

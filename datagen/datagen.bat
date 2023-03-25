2ed2a4ed5f685b4455573b1ee5423b4f0e8007c0
crippa1337 <casdcasd5@gmail.com>
2023-03-26 00:04:03 +0100

    uho info

--- datagen/datagen.bat
+++ datagen/datagen.bat
@@ -8,3 +8,5 @@ cutechess-cli -engine cmd=target/release/svart.exe -engine cmd=target/release/sv
     -rounds 284039 -games 1 -concurrency 10 ^
     -openings file=datagen/UHO_XXL_2020.pgn format=pgn order=sequential plies=10 ^
     -pgnout datagen/gigadata.pgn
+
+rem 284039 is the amount of lines in the UHO_XXL_2020 book

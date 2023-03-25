b6dd26a5a563ee9a8f23fe7c8849f125aac1c146
crippa1337 <casdcasd5@gmail.com>
2023-03-25 23:54:11 +0100

    first net

--- datagen/pgn_to_marlinflow.py
+++ datagen/pgn_to_marlinflow.py
@@ -1,44 +1,60 @@
+# A script to convert PGN files to the marlinflow format
+# FEN | EVAL | WDL
+
+
 import chess
 import chess.pgn
+import sys
 
-positions = 0
-games = 0
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python pgn_to_marlinflow.py <pgn_file> <output_file>")
+        sys.exit(1)
 
-with open("input.pgn", "r") as pgn:
-    with open("output.txt", "a") as output:
-        while True:
-            game = chess.pgn.read_game(pgn)
-            
-            # EOF
-            if game is None:
-                break
-            
-            games += 1
-            
-            result = game.headers["Result"]
-            if result == "1-0":
-                wdl = "1"
-            elif result == "0-1":
-                wdl = "0"
-            else:
-                wdl = "0.5"
-            
-            board = game.board()
-            for position in game.mainline():
-                eval = position.comment.split("/")[0]
+    pgn_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    positions = 0
+    games = 0
+
+    with open(pgn_file, "r") as pgn:
+        with open(output_file, "a") as output:
+            while True:
+                game = chess.pgn.read_game(pgn)
                 
-                if "M" in eval:
+                # EOF
+                if game is None:
                     break
                 
-                if not "book" in eval and not board.is_check() and eval != "":                    
-                    eval = int(float(eval) * 100)
-                    if board.turn == chess.BLACK:
-                        eval = -eval
+                games += 1
+                
+                result = game.headers["Result"]
+                if result == "1-0":
+                    wdl = "1"
+                elif result == "0-1":
+                    wdl = "0"
+                else:
+                    wdl = "0.5"
+                
+                board = game.board()
+                for position in game.mainline():
+                    eval = position.comment.split("/")[0]
                     
-                    output.write(f"{board.fen()} | {eval} | {wdl}\n")
-                    positions += 1
+                    if "M" in eval:
+                        break
                     
-                    if positions % 1000000 == 0:
-                        print(f"Processed {positions / 1000000}M positions and {games} games")
+                    if not "book" in eval and not board.is_check() and eval != "":                    
+                        eval = int(float(eval) * 100)
+                        if board.turn == chess.BLACK:
+                            eval = -eval
                         
-                board.push(position.move)
+                        output.write(f"{board.fen()} | {eval} | {wdl}\n")
+                        positions += 1
+                        
+                        if positions % 1000000 == 0:
+                            print(f"Processed {positions / 1000000}M positions and {games} games")
+                            
+                    board.push(position.move)
+                    
+if __name__ == "__main__":
+    main()

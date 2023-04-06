b114a9c3163b117a77b0aa0bfca0cce30a45ef9c
crippa1337 <casdcasd5@gmail.com>
2023-04-06 19:29:34 +0200

    Better repeating timeman
    40/20 50+-25 elo

--- src/uci/timeman.rs
+++ src/uci/timeman.rs
@@ -1,23 +1,28 @@
 use crate::definitions::TIME_OVERHEAD;
 
+#[must_use]
 pub fn time_for_move(time: u64, inc: u64, moves_to_go: Option<u8>) -> (u64, u64) {
     // Accounting for overhead
     let time = time - TIME_OVERHEAD;
-    let opt_time: u64;
-    let max_time: u64;
+    let opt_time: f64;
+    let max_time: f64;
 
-    // movestogo...
-    if let Some(n) = moves_to_go {
-        opt_time = time / n.max(1) as u64;
-        max_time = opt_time
-    // wtime ... winc...
+    // repeating TC
+    if let Some(mtg) = moves_to_go {
+        let mtg = mtg.min(50);
+        let scale = 0.7 / f64::from(mtg);
+        let eight = 0.8 * time as f64;
+
+        opt_time = (scale * time as f64).min(eight);
+        max_time = (5. * opt_time).min(eight);
+    // normal TC
     } else {
-        let temp = (time / 20) + (inc / 2);
-        opt_time = (6 * temp) / 10;
-        max_time = time.min(temp * 2);
+        let temp = ((time / 20) + (inc / 2)) as f64;
+        opt_time = 0.6 * temp;
+        max_time = (temp * 2.).min(time as f64);
     }
 
     // The optimum time is used right after a depth is cleared in the ID loop.
     // Max time is used in the search function as usual for a global stop light.
-    (opt_time, max_time)
+    (opt_time as u64, max_time as u64)
 }

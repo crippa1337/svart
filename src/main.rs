1f43b8f3f5f04abfbe1ef74ec98b041583ece653
crippa1337 <casdcasd5@gmail.com>
2023-05-01 13:52:06 +0200

    Move some stuff around
    Bench: 11677845

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,25 @@ mod definitions;
 mod engine;
 mod uci;
 
-use uci::handler::uci_loop;
-
 fn main() {
     #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
     let binding = std::env::args().nth(1);
     let arg = binding.as_deref();
-    if arg == Some("bench") {
-        uci::bench::bench();
-        return;
-    } else if arg == Some("table") {
-        engine::nnue::tables::print_net_history();
-        return;
+    match arg {
+        Some("bench") => {
+            uci::bench::bench();
+            return;
+        }
+        Some("table") => {
+            engine::nnue::tables::print_net_history();
+            return;
+        }
+        Some("datagen") => {
+            engine::nnue::datagen::script::root().unwrap();
+            return;
+        }
+        _ => {}
     }
 
-    uci_loop();
+    uci::handler::uci_loop();
 }

00c6fc72958092f5d0ef89611dcf820532127720
crippa1337 <casdcasd5@gmail.com>
2023-05-31 17:30:43 +0200

    mroe stuff

--- engine/src/body/see.rs
+++ engine/src/body/see.rs
@@ -4,23 +4,42 @@ use super::position::is_ep;
 
 const VALUES: [i32; 6] = [100, 300, 330, 500, 900, 0];
 
-// fn see(board: &Board, mv: Move, threshold: i32) -> bool {
-//     let stm = board.side_to_move();
+fn see(board: &Board, mv: Move, threshold: i32) -> bool {
+    // Value of the move minus the threshold
+    let mut score = move_gain(board, mv) - threshold;
+    
+    let next_victim = if let Some(promo) = mv.promotion {
+        promo
+    } else {
+        board.piece_on(mv.from).unwrap()
+    };
 
-//     let score = move_gain(board, mv) - threshold;
+    // Best case, no more captures
+    // Fails to beat threshold
+    if score < 0 {
+        return false;
+    }
 
-//     // Threshold breaks the gain
-//     if score < 0 {
-//         return false;
-//     }
+    // Worst case, the opponent captures the piece back
+    score -= VALUES[next_victim as usize];
 
-//     false
-// }
+    // Worst case beats the threshold
+    if score >= 0 {
+        return true;
+    }
+
+    let bishops = board.pieces(Piece::Bishop) | board.pieces(Piece::Queen);
+    let rooks = board.pieces(Piece::Rook) | board.pieces(Piece::Queen);
+    let stm = board.side_to_move();
+ 
+
+    false
+}
 
 fn move_gain(board: &Board, mv: Move) -> i32 {
     // Castling
-    if board.piece_on(mv.from) == Some(Piece::King) && mv.from.file() < mv.to.file() {
-        return 0;
+    if board.color_on(mv.to).is_some() {
+        return 0
     }
 
     if is_ep(board, mv) {
